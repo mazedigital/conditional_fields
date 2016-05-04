c6ea39c3a07a31e6af920f90c064ff052d8379f8
Jon Mifsud <info@jonmifsud.com>
2016-05-04 12:20:06 +0200

    release 0.2 taking care of setting filters in backend

--- extension.driver.php
+++ extension.driver.php
@@ -10,6 +10,16 @@ class extension_conditional_fields extends Extension {
 				'delegate' => 'InitialiseAdminPageHead',
 				'callback' => 'initializeAdmin',
 			),
+			array(
+				'page' => '/publish/',
+				'delegate' => 'AssociationFiltering',
+				'callback' => 'associationFiltering'
+			),
+			array(
+				'page' => '/publish/edit/',
+				'delegate' => 'EntryPreRender',
+				'callback' => 'entryPreRender'
+			),
 		);
 	}
 	
@@ -38,6 +48,107 @@ class extension_conditional_fields extends Extension {
 			$page->addScriptToHead($assets_path . '/js/conditional-fields.js', $LOAD_NUMBER++);
 		}
 		
+	}
+
+	/**
+	 * Entry Pre Render
+	 */
+	public function entryPreRender($context) {
+
+		$entry = $context['entry'];
+		$section = $context['section'];
+		$section_id = $section->get('id');
+
+		$conditionalFields = Symphony::Configuration()->get($section->get('handle'),'conditional_fields');
+
+		// var_dump($conditionalFields);die;
+		$conditionalFields = json_decode($conditionalFields);
+
+		$matchedCondition = NULL;
+
+		if (isset($conditionalFields)){
+			foreach ($conditionalFields as $rule) {
+				// var_dump($rule->condition);die;
+				$match = true;
+				foreach ($rule->condition as $field => $value) {
+					if (!isset($this->fieldID[$field])){
+						$this->fieldID[$field] = FieldManager::fetchFieldIDFromElementName($field,$section_id);
+					}
+
+					$fieldData = $entry->getData($this->fieldID[$field]);
+					if ( $fieldData['handle'] == $value || $fieldData['value'] == $value || $fieldData['relation_id'] == $value){
+						//this is a match
+					} else {
+						// this is not a match
+						$match = false;
+					}
+				}
+
+				if ($match){
+					$this->matchedCondition = get_object_vars($rule);
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * Filter select choices - this ensures that users see only data according to the conditions set
+	 */
+	public function associationFiltering($context) {
+
+		if (!isset($context['field-id']) || !isset($this->matchedCondition[$context['field-id']])){
+			//stop as there are no conditions met or field set
+			return;
+		}
+
+		$filters = get_object_vars($this->matchedCondition[$context['field-id']]);
+		if (!isset($filters['filters'])){
+			// if there are no usable filters (just a hide/show) no association filtering needs to happen
+			return;
+		}
+
+		$filters =  get_object_vars($filters['filters']);
+
+		if (isset($context['filters']) && isset($filters)){
+			$context['filters'] = $filters;
+		}
+
+		//Note: the below filtering doesn't work properly if you have multiple linked sections - might need to invest more time for such scenarios
+		if (!empty($filters)) {
+			// Build Filters
+
+			$field = FieldManager::fetch($context['field-id']);
+			$relatedFieldID = $field->get('related_field_id');
+
+			$relatedField = current(FieldManager::fetch($relatedFieldID));
+			$relatedSectionID = $relatedField->get('parent_section');
+
+			$joins = "";
+			$where = "";
+
+			foreach ($filters as $handle => $value) {
+				if (!is_array($value)) {
+					$filter_type = Datasource::determineFilterType($value);
+					$value = preg_split('/'.($filter_type == Datasource::FILTER_AND ? '\+' : '(?<!\\\\),').'\s*/', $value, -1, PREG_SPLIT_NO_EMPTY);
+					$value = array_map('trim', $value);
+					$value = array_map(array('Datasource', 'removeEscapedCommas'), $value);
+				}
+
+				$handle = Symphony::Database()->cleanValue($handle);
+				$filter_id = FieldManager::fetchFieldIDFromElementName($handle,$relatedSectionID);
+
+				$field = FieldManager::fetch($filter_id);
+				if ($field instanceof Field) {
+					$field->buildDSRetrievalSQL($value, $joins, $where, ($filter_type == Datasource::FILTER_AND ? true : false));
+				}
+			}
+
+			$context['where'] .= $where;
+			$context['joins'] .= $joins;
+
+		}
+
 	}
 		
 	//todo add preferences page to add the settings for now use the below for a guide on what needs to be added within the config
